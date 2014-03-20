6421c7e38f3cf71fa6ca5dcb17345f477dc96e48
Slava <slavapas13@gmail.com>
2014-03-20 21:10:21 +0200

    Rolback

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -33,8 +33,4 @@ SampleAppsl::Application.configure do
 
   # Print deprecation notices to the stderr.
   config.active_support.deprecation = :stderr
-  
-  # Speed up tests by lowering bcrypt's cost function.
-  ActiveModel::SecurePassword.min_cost = true
-
 end
